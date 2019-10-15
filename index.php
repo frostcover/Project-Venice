9527c035b9d34d7c3d91f9fc0cc14f128e4570a4
Shubhesh Dwivedi <shubheshdwivedi@ameyo.com>
2019-10-15 15:19:08 +0530

    design changed with green theme

--- index.php
+++ index.php
@@ -60,6 +60,10 @@ if(isset($_GET['query'])) {
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,500,700" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="design.css">
+    <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <div class="container-fluid">
